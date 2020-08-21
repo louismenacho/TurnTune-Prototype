3a821758bd4b22d598be9ddcdfb3d009b1e6309d
louismenacho <menacho.louis@gmail.com>
2020-08-21 00:12:42 -0400

    Add Spotify iOS SDK

---
+++ TurnTune/TurnTune-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  TurnTune-Bridging-Header.h
+//  TurnTune
+//
+//  Created by Louis Menacho on 8/20/20.
+//  Copyright © 2020 Louis Menacho. All rights reserved.
+//
+
+#import <SpotifyiOS/SpotifyiOS.h>
